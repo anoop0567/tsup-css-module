4c6db1c6b15078318d5a869f2bec75e2275e21e6
shanti-qr <chauhananu0567@gmail.com>
2025-01-30 05:37:12 +0530

    chore: splitting false

--- packages/design-system/tsup.config.ts
+++ packages/design-system/tsup.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/components/index.ts", "src/tokens/index.css"],
+  entry: ["src/**/*.ts", "src/tokens/index.css"],
   dts: true,
   clean: true,
   format: ["esm"],
-  external: ["react", "react-dom", "class-variance-authority"],
   loader: {
     ".css": "copy",
   },
+  splitting: false,
+  external: ["react", "react-dom", "class-variance-authority"],
 });
